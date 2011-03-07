0e228ea082ec3c04af8d3fb99e96e4084578acc4
flomavali <flomavali@3934691f-ceb8-4060-a2e4-3745ab6387a9>
2011-03-07 08:50:02 +0000

    Add intellisense support. http://www.ilch.de/forum-showposts-44547-p1.html#314444

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends MY_Controller {
 
 	function __construct()
 	{
---
+++ application/libraries/MY_Controller.php
@@ -0,0 +1,109 @@
+<?php
+class MY_Controller extends Controller
+{
+	/**
+	 * @var CI_User_agent
+	 */
+ 	var $agent;
+	/**
+	 * @var CI_Benchmark
+	 */
+ 	var $benchmark;
+	/**
+	 * @var CI_Calendar
+	 */
+ 	var $calendar;
+	/**
+	 * @var CI_Cart
+	 */
+ 	var $cart;
+	/**
+	 * @var CI_Config
+	 */
+ 	var $config;
+	/**
+	 * @var CI_DB_driver
+	 */
+ 	var $db;
+	/**
+	 * @var CI_Email
+	 */
+ 	var $email;
+	/**
+	 * @var CI_Encrypt
+	 */
+ 	var $encrypt;
+	/**
+	 * @var CI_Form_validation
+	 */
+ 	var $form_validation;
+	/**
+	 * @var CI_FTP
+	 */
+ 	var $ftp;
+	/**
+	 * @var CI_Image_lib
+	 */
+ 	var $image_lib;
+	/**
+	 * @var CI_Input
+	 */
+ 	var $input;
+	/**
+	 * @var CI_Language
+	 */
+ 	var $lang;
+	/**
+	 * @var CI_Output
+	 */
+ 	var $output;
+	/**
+	 * @var CI_Pagination
+	 */
+ 	var $pagination;
+	/**
+	 * @var CI_Parser
+	 */
+ 	var $parser;
+	/**
+	 * @var CI_Session
+	 */
+ 	var $session;
+	/**
+	 * @var CI_Table
+	 */
+ 	var $table;
+	/**
+	 * @var CI_Trackback
+	 */
+ 	var $trackback;
+	/**
+	 * @var CI_Typography
+	 */
+ 	var $typography;
+	/**
+	 * @var CI_Unit_test
+	 */
+ 	var $unit;
+	/**
+	 * @var CI_Upload
+	 */
+ 	var $upload;
+	/**
+	 * @var CI_URI
+	 */
+ 	var $uri;
+	/**
+	 * @var CI_Xmlrpc
+	 */
+ 	var $xmlrpc;
+	/**
+	 * @var CI_Xmlrpcs
+	 */
+ 	var $xmlrpcs;
+	/**
+	 * @var CI_Zip
+	 */
+ 	var $zip;
+}
+?>
