bccf6e45c282ad8d5d51857f7ece4adbc7618085
Flomavali <Flomavali@3934691f-ceb8-4060-a2e4-3745ab6387a9>
2011-03-09 20:40:20 +0000

    Einmal alles resetten. Mein SVN-Client hat immer noch nicht die Kurve gekriegt.

--- application/logs/2011/03/07.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2011-03-07 13:09:29 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL ilchcms2x/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 94 ]
-2011-03-07 20:14:08 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL ilchcms2x/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 94 ]
-2011-03-07 20:16:05 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL ilchcms2x/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 94 ]
