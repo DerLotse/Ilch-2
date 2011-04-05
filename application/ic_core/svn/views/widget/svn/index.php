c273357877c0b3543ea2e3709d292d9e35876bb7
flomavali <flomavali@3934691f-ceb8-4060-a2e4-3745ab6387a9>
2011-04-05 18:20:56 +0000

    Add: Update-Link to SVN-Widget

--- application/ic_core/svn/views/widget/svn/index.php
+++ application/ic_core/svn/views/widget/svn/index.php
@@ -1,3 +1,6 @@
 <p>
     Die Datenbank ist auf dem Stand der <b>Revision <?php echo $revision; ?></b>.
 </p>
+<p>
+    <a href="<?php echo URL::site('backend/svn'); ?>">Datenbankupdate starten</a>
+</p>
