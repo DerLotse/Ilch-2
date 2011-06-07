9b8ee5b244a85c514cd0c49e782bf3fe6f132188
flomavali <flomavali@3934691f-ceb8-4060-a2e4-3745ab6387a9>
2011-06-07 17:23:57 +0000

    Ein paar Änderungen aus den letzten Wochen. Nur Optimierungsarbeiten. Nun geht's an das Auth-System!

--- contents/core/modules/ilchcms/classes/ilch.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Ilch extends Ilch_Core {}
--- contents/core/modules/media/init.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-// Static file serving (CSS, JS, images)
-Route::set('media', 'media(/<file>)', array('file' => '.+'))
-	->defaults(array(
-		'controller' => 'media',
-		'action'     => 'index',
-		'file'       => NULL,
-	));
