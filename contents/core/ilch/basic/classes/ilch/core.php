81c48878a32046ccaed0ab7708a985f026482a08
KoernerWS <contact@florian-koerner.eu>
2011-09-08 16:21:34 +0200

    Dynamische Module - Teil 3
    - Cache-Funktion wieder entfernt, da für die eine Datenbank-Abfrage
    nicht nötig - Weitere Funktionen aus dem Test-Model übernommen und
    angepasst  - install_test.php entfernt, da nicht genutzt

--- contents/core/ilch/basic/classes/ilch/core.php
+++ contents/core/ilch/basic/classes/ilch/core.php
@@ -8,6 +8,14 @@ class Ilch_Core {
 	const CODENAME = 'Pluto';
 
 	public static $caching = FALSE;
+	
+	public static $modules = array();
+	
+	public static $module_version = array();
+	
+	protected static $_module_cache = array();
+	
+	protected static $_module_position = array();
 
 	/**
 	 * Load all needed modules and themes and set needed config
@@ -23,8 +31,6 @@ class Ilch_Core {
 		
 		// Get modules
 		Ilch::load_modules();
-		
-		echo Ilch::module_path('ilch_svn');
 		
 		/*
 		// Attach a database reader to config.
@@ -45,28 +51,113 @@ class Ilch_Core {
 	}
 	
 	/**
-	 * 
-	 * Enter description here ...
+	 * Load all active Modules from the Database
 	 */
 	public static function load_modules()
 	{
-		// Load cache data
-		$cache = (Ilch::$caching) ? Kohana::cache('Ilch::load_modules()') : NULL;
+		// Load database module
+		Kohana::modules(array('kohana_database' => MODPATH.'core'.DIRSEPA.'kohana'.DIRSEPA.'database'));
 		
-		// If cache invalid
-		if (!$cache)
+		// Get all active modules
+		Ilch::$_module_cache = DB::select('module_name', 'module_version')->from('modules')->execute()->as_array('module_name', 'module_version');
+			
+		// If cache valid
+		if (Ilch::$_module_cache)
 		{
-			// Load database module
-			Kohana::modules(array('kohana_database' => MODPATH.'core'.DIRSEPA.'kohana'.DIRSEPA.'database'));
-			
-			// Get all active modules
-			$query = DB::select('module_name', 'module_version')->from('modules')->execute();
-			
-			foreach ($query AS $row)
+			foreach(Ilch::$_module_cache AS $module => $version)
 			{
-				print_r($row);
+				Ilch::load_module($module, $version);
 			}
 		}
+		
+		// Load modules into kohana
+		Ilch::update_modules();
+	}
+	
+	/**
+	 * Load given module
+	 */
+	public static function load_module($module, $version)
+	{	
+		// Abort if module already loaded
+		if (isset(Ilch::$modules[$module])) return TRUE;
+		
+        // Load module path
+		$module_path = Ilch::module_path($module);
+		
+		// Check module directory
+		if (!is_dir($module_path)) return FALSE;
+		
+		// Load configuration
+		$config = Arr::get((file_exists($module_path.'config'.DIRSEPA.'module'.EXT)) ? Kohana::load($module_path.'config'.DIRSEPA.'module'.EXT) : array(), $module, array());
+		
+		// Check module version
+		if (!empty($version) AND Ilch::version_compare($version, '<', Arr::get(Arr::get($config, 'details', array()), 'version', 0)))
+		{
+			// @todo Automatisches installieren neuer Datenbankeinträge
+		}
+		else if(!empty($version) AND Ilch::version_compare($version, '>', Arr::get(Arr::get($config, 'details', array()), 'version', 0)))
+		{
+			return FALSE;
+		}
+		
+		// Save version number
+		Ilch::$module_version[$module] = (!empty($version)) ? Arr::get(Arr::get($config, 'details', array()), 'version', 0) : 0;
+		
+        // Module position
+        $position = NULL;
+		
+	    // Place using required data
+        if (isset($config['required']))
+        {
+            foreach ($config['required'] AS $required_module => $required_version)
+            {
+				// If module is not loaded
+				if (!isset(Ilch::$modules[$required_module]) AND !Ilch::load_module($required_module, $required_version))
+				{
+					return FALSE;
+				}
+				else if (isset(Ilch::$modules[$required_module]) AND true)
+				{
+					// @todo Hier weiterarbeiten ;)
+				}
+            }
+        }
+		
+        // Place using extends data
+        if (isset($config['extends']))
+        {
+            foreach ($config['extends'] AS $extend)
+            {
+                if (isset(Ilch::$_module_position[$extend]) AND ($position === NULL OR Ilch::$_module_position[$extend] < $position))
+                {
+                    $position = Ilch::$_module_position[$extend];
+                }
+            }
+        }
+		
+		// Place module finaly
+        if ($position === NULL)
+        {
+            Ilch::$modules[$module] = $module_path;
+        }
+        else
+        {
+            Ilch::$modules[$modules] = Ilch::module_place($module, $module_path, $position);
+        }
+				
+		// Set new module positions
+		Ilch::$_module_position = array_flip(array_keys(Ilch::$modules));
+		
+		return TRUE;
+	}
+	
+	/**
+	 * Send loaded modules to Kohana
+	 */
+	public static function update_modules()
+	{
+		Kohana::modules(Ilch::$modules);
 	}
 	
 	/**
@@ -82,7 +173,7 @@ class Ilch_Core {
 		// Check data
 		if (count($module_arr) == 1)
 		{
-			return ((is_dir(APPPATH.'modules'.DIRSEPA.$module_arr[0])) ? APPPATH : MODPATH).'modules'.DIRSEPA.$module_arr[0];
+			return ((is_dir(APPPATH.'modules'.DIRSEPA.$module_arr[0])) ? APPPATH : MODPATH).'modules'.DIRSEPA.$module_arr[0].DIRSEPA;
 		}
 		else
 		{
@@ -99,6 +190,67 @@ class Ilch_Core {
 	{
 		return ((is_dir(APPPATH.'themes'.DIRSEPA.$theme_name)) ? APPPATH : MODPATH).'themes'.DIRSEPA.$theme_name;
 	}
+
+
+	/**
+	 * Compare two Versions with the Ilch 2 standard version syntax
+	 *
+	 *     Ilch::compare_version('1.3.4 Beta 1', '<', '1.3.4 Alpha 3'); // Return false
+	 *
+	 *     Ilch::compare_version('1.3.4 Beta 1', '<', '1.3.4'); // Return true
+	 *
+	 * 
+	 */
+	public static function version_compare($act_version, $compare_operator, $new_version)
+	{
+		// Create full version number
+		$act_version = Ilch::version_number($act_version);
+		$act_version = Ilch::version_number($new_version);
+	
+		// Check version number
+		switch($compare_operator) {
+			case '>': return ($act_version > $new_version); break;
+			case '<': return ($act_version < $new_version); break;
+			case '>=': return ($act_version >= $new_version); break;
+			case '<=': return ($act_version <= $new_version); break;
+			case '==': return ($act_version == $new_version); break;
+		}
+		
+		return FALSE;
+	}
+	
+	/**
+	 * Get the integer value of a string with the Ilch 2 standard version syntax
+	 */
+	public static function version_number($version)
+	{
+		// Named versions
+		$named_versions = array(
+			'development' => 0,
+			'dev' => 0,
+			'prealpha' => 1,
+			'alpha' => 2,
+			'beta' => 3,
+			'releasecandidate' => 4,
+			'rc' => 4
+		);
+		
+		// Split versions
+		$version = explode(' ', trim($version));
+		$version[0] = explode('.', $version[0]);
+				
+		$version = array(
+			0 => Arr::get($version[0], 0, 0),
+			1 => Arr::get($version[0], 1, 0),
+			2 => Arr::get($version[0], 2, 0),
+			3 => (isset($version[1])) ? Arr::get($named_versions, strtolower($version[1]), 0) : 0,
+			4 => Arr::get($version, 2, 0)
+		);
+		
+		// Format and return
+		return sprintf('%d.%03d%03d%03d%03d', $version[0], $version[1], $version[2], $version[3], $version[4]);
+	}
+	
 
 	/**
 	 * Dynamic Ilch CMS route
---
+++ contents/modules/welcome/config/module.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+    'welcome' => array(
+        'details' => array(
+            'name' => __('Basic Ilch Module/System'),
+            'description' => '',
+            'version' => '2.0',
+        ),
+        'resources' => array(
+            'download_server' => 'http://download.ilch-pluto.net/server/ilch-basic/',
+            'download_page' => 'http://download.ilch-pluto.net/modules/ilch-basic/',
+            'homepage' => 'http://www.ilch.net/',
+            'documentation' => 'https://github.com/IlchCMS/Ilch-2/wiki/ilch-basic-module',
+            'support' => 'http://www.ilch.net/',
+        ),
+        'authors' => array(
+            array(
+                'name' => 'Florian Körner',
+                'description' => '',
+                'website' => 'florian-koerner.eu',
+                'email' => 'support@florian-koerner.eu',
+                'company' => 'Florian Körner Weblösungen',
+                'address' => '',
+                'city' => 'Bad Segeberg',
+                'zip_code' => 23795,
+                'staate' => __('Germany'),
+            ),
+        ),
+        'required' => array('ilch_media' => 1),
+        'expand' => array('core_kohana_modules_database' => 1, 'modules_welcome' => 1
+		)
+    )
+);
